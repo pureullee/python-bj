531e4e9f4d25be6be706ad91a041c80c6b17691b
pureullee <pureullee@gmail.com>
2023-03-07 17:18:22 +0900

    재귀 아니게

--- Selection_sort.py
+++ Selection_sort.py
@@ -1,43 +1,26 @@
-import sys
-
+#23881
+import sys 
 
 result_list = []
 
-def selection_sort(a_list, n) :
-    
-    Max = a_list[0] 
-    j=n-1
-    for i in range(0, n) :
-                
-        if Max < a_list[i] :
-            Max = a_list[i] 
-            j = i
-        
-        
-            
-    if Max != a_list[n-1] :
-        if j != n-1 :
-            a_list[j] = a_list[n-1]
-            a_list[n-1] = Max
-            result_list.append([a_list[j], Max])
-        else :
-            a_list[0] = a_list[n-1]      
-            a_list[n-1] = Max
-            result_list.append([a_list[0], Max])
-        
-    if n > 1 :            
-        selection_sort(a_list, n-1)
-                
-
 n, k = map(int,input().split())
-data = list(map(int, sys.stdin.readline().split()))   
-
-selection_sort(data, n)
+data = list(map(int, sys.stdin.readline().split()))
+count = 0
+while True :
+    Max = max(data[0:n-count])
+    i = data.index(Max)
+    if data[-1-count] != Max :
+        data[i] = data[-1-count]
+        data[-1-count] = Max
+        result_list.append([data[i],Max])
+    count += 1
+    if count >= n-1 : break
 
 if len(result_list) < k :
     print(-1)
 else :
     print(result_list[k-1][0], result_list[k-1][1])
     
+
 
                 
