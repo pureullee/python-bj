86f8af149849bd7ebd4c6b8f034d5b781bf2f7e5
pureullee <pureullee@gmail.com>
2023-03-24 17:53:46 +0900

    pop()할때 O(1)의 시간복잡도 가지게 수정

--- Linkedlist.py
+++ Linkedlist.py
@@ -51,22 +51,25 @@ class LinkedList:
     def append(self, value):
         new_node = Node(value)
         self.tail.next = new_node
+        self.tail_pre = self.tail # tail 바로 이전의 노드 
         self.tail = new_node
         self.count += 1
         
         
     def insert(self,value,inx) :
         perv_node = self.head
-        self.ptr = 1
-        if inx <= self.count :
+        self.ptr = 0
+        if inx <= self.count-1 :
             while self.ptr != inx :
                 perv_node = perv_node.next
                 self.ptr += 1
             new_node = Node(value,perv_node.next)
             perv_node.next = new_node
+            if inx == self.count -1 : #tail 바로 이전의 삽입 경우엔 tail_pre를 갱신해줘야함
+                self.tail_pre = new_node
             self.count +=1
             
-    def pop(self) :
+    def pop(self) : #이경우엔 항상 o(n)의 복잡도를 가져 비효율적
         perv_node = self.head
         self.ptr = 1 
         while self.ptr != self.count :
@@ -78,6 +81,15 @@ class LinkedList:
         self.tail = perv_node
         self.count -= 1
         return result.value
+    
+    def pop_new(self) :
+        
+        perv_node = self.tail_pre
+        result = self.tail.value
+        perv_node.next = None
+        self.tail = perv_node 
+        self.count -=1
+        return result
             
    
             
@@ -89,8 +101,9 @@ ls = LinkedList()
 ls.append(3)
 ls.append(5)
 ls.insert(2,1)
-print(ls.pop())
+print(ls.pop_new())
 ls.append(7)
+ls.insert(0,0)
 ls.print()
 
         
